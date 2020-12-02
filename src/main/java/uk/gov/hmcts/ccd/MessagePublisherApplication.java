c34a55bb0fe394c5f4b8106247999c746719b423
Dan Lysiak <daniel.lysiak@cgi.com>
2020-12-02 10:02:48 +0000

    Unused import

--- src/main/java/uk/gov/hmcts/ccd/MessagePublisherApplication.java
+++ src/main/java/uk/gov/hmcts/ccd/MessagePublisherApplication.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.ccd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
