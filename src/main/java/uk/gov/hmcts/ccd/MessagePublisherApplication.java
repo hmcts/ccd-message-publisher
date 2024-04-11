f3248537ac575a8b9e7c6e1d89141774bfd61023
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2024-04-11 18:12:28 +0100

    Revert "CCD-5316 - Use latest Service Bus library"

--- src/main/java/uk/gov/hmcts/ccd/MessagePublisherApplication.java
+++ src/main/java/uk/gov/hmcts/ccd/MessagePublisherApplication.java
@@ -2,10 +2,8 @@ package uk.gov.hmcts.ccd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
-@EnableJms
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class MessagePublisherApplication {
 
