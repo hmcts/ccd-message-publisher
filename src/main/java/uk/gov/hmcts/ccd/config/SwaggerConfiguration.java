06b0586d090f80ffd056f04fcf22d9c74fa90337
JamesCollettCGI <james.collett@hmcts.net>
2026-05-28 19:36:35 +0100

    Update SwaggerConfiguration.java

--- src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/config/SwaggerConfiguration.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.ccd.config;
 
-import jakarta.jms.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -17,12 +15,5 @@ public class SwaggerConfiguration {
               .info(new Info().title("CCD Message Publisher API")
               .description("CCD Message Publisher API")
               .version("v0.0.1"));
-    }
-
-    @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(final ConnectionFactory connectionFactory) {
-        final DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        return factory;
     }
 }
